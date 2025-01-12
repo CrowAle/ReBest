ad78fc675a9151a85ed3ad371ca70752649e053b
CrowAle <infoopportunitadiguadagno@gmail.com>
2025-01-12 21:31:05 +0100

    third commit build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+experimental: {
+  serverActions: true,
+},
 };
 
 export default nextConfig;
