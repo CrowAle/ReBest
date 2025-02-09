6c8e650f11c4c21f749b0650e917885a2eb3d260
CrowAle <infoopportunitadiguadagno@gmail.com>
2025-02-09 16:28:05 +0100

    conMaps

--- app/components/OSMMapComponent copy.tsx
+++ app/components/OSMMapComponent copy.tsx
@@ -53,3 +53,9 @@ const OSMMapComponent: React.FC<OSMMapComponentProps> = ({ locations }) => {
 
 export default OSMMapComponent;
 
+var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+});
